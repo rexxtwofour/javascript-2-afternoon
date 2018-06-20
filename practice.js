83be3d850b8de6405571649a41705528932a7631
Eric Rivera <ericrivera@Eric-Riveras-MacBook-Pro.local>
2018-06-19 18:45:15 -0700

    initial

--- practice.js
+++ practice.js
@@ -16,8 +16,12 @@
 
 //Code here
 
+let me = {
+  name:"your name",
+  age:"your age",
+}
 
-
+alert.name;
 ////////// PROBLEM 2 //////////
 
 /*
@@ -26,7 +30,14 @@
 */
 
 //Code here
-
+let favoriteThings = {
+  band:"kiss",
+  food:"pizza",
+  person:"brianna",
+  book:"outlier",
+  movie:"tron",
+  holiday:"halloween"
+};
 
 
 /*
@@ -35,14 +46,16 @@
 
 //Code here
 
-
+favoriteThings.car ="corvette";
+favoriteThings.brand ="chevrolet";
 
 /*
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
-
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 
 ////////// PROBLEM 3 //////////
@@ -56,16 +69,19 @@
 */
 
 //Code here
+let backPack = {
 
-
-
+}
+let item = "firstPocket";
+backPack[item]="chapstick";
+backPack.color ="color";
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
 
-
+//alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -75,7 +91,7 @@ Instead, console.log your whole backPack object and then check out the console.
 
 //Code here
 
-
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
@@ -92,13 +108,14 @@ var user2 = {
 
 /* 
   Let's say I, the user, decided to change my name and email address to the following:
-  name -> 'Tyler S. McGinnis' and email -> 'tyler.mcginnis@devmounta.in'. 
+  name -> 'Tyler S. McGinnis' and email -> 'c'. 
   Make that change without modifying the original object code above.
 */
 
 //Code Here
 
-
+user2.name = "Tyler S. McGinnis";
+user2.email = "tyler.mcginnis@devmounta.in";
 
 
 
@@ -120,17 +137,23 @@ var user2 = {
 */
 
 //Code Here
+let methodCollection = {
 
+}
 
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object. 
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console. 
 */
-
+//let obj = {
+//  alertHello: function () {
+//   alert.alerthello;
+// } 
+//}
+methodCollection.alertHello = function(){window.alert('hello')}
+methodCollection.logHello = function(){console.log('hello')}
 //Code Here
-
-
 
 /*
   Now call your alertHello and logHello methods.
@@ -138,8 +161,8 @@ var user2 = {
 
 //Code Here
 
-
-
+methodCollection.alertHello;
+methodCollection.logHello;
 ////////// PROBLEM 6 //////////
 
 /* 
@@ -148,6 +171,13 @@ var user2 = {
 */
 
 //Code Here
+function makePerson(name,birthday,ssn){
+
+return {
+  name,
+  birthday,
+  ssn};
+}
 
 
 
@@ -159,5 +189,15 @@ var user2 = {
 */
 
 //Code Here
+
+
+
+function makeCard(){creditCard={
+  cardNumber: "123", 
+  expirationDate: "456", 
+  securityCode: "666"};
+
+return (creditCard);
+}
 
 
